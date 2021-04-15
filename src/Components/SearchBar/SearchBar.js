f73fddb9472d9f234785fe54c7245524c2131cca
ssullivan <samuel.b.sullivan@gmail.com>
2021-04-15 13:38:04 -0500

    Added Playlist and SearchResults components

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,12 @@
 import './SearchBar.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export class SearchBar extends React.Component {
   render() {
     return (
       <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" />
-        <button class="SearchButton">SEARCH</button>
+        <button className="SearchButton">SEARCH</button>
       </div>
     );
   }
